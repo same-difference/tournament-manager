1a04e2e003cb47bf7a5de6af0af93e9b74f04321
JacobStudstill <jacob.studstill@gmail.com>
2024-03-04 13:43:50 -0500

    Changed color of search button and added a / to the routes on app.tsx

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -15,27 +15,29 @@ export default function NavBar() {
                                 <a className="nav-link active" aria-current="page" href="#">Home</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">Dashboard</a>
+                                <a className="nav-link" href="/dashboard">Dashboard</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link disabled" aria-disabled="true">Tourney</a>
+                                <a className="nav-link" href="/tourney">Tourney</a>
                             </li><li className="nav-item">
-                                <a className="nav-link disabled" aria-disabled="true">Match</a>
+                                <a className="nav-link" href="/match">Match</a>
                             </li>
                         </ul>
                         <form className="d-flex p-2" role="search">
                             <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                                <button className="btn btn-outline-success" type="submit">Search</button>
+                                <button className="btn btn-outline-primary" type="submit">Search</button>
                         </form>
                         <a className="nav-link dropdown-toggle p-2" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                     Profile
                                 </a>
                                 <li className="nav-item dropdown">
                                 <ul className="dropdown-menu dropdown-menu-end">
-                                    <li><a className="dropdown-item" href="#">Action</a></li>
-                                    <li><a className="dropdown-item" href="#">Another action</a></li>
+                                    <li><a className="dropdown-item" href="/login">Login / Sign up</a></li>
                                     <li><hr className="dropdown-divider"/></li>
-                                    <li><a className="dropdown-item" href="#">Something else here</a></li>
+                                    <li><a className="dropdown-item" href="#">Profile</a></li>
+                                    <li><a className="dropdown-item" href="#">Account</a></li>
+                                    <li><hr className="dropdown-divider"/></li>
+                                    <li><a className="dropdown-item" href="#">Settings</a></li>
                                 </ul>
                                 </li>
                     </div>
